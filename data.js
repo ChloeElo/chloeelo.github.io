fa2a98bcf820d33dddd695e74bfdf692e7b4b534
ChloeElo <105892909+ChloeElo@users.noreply.github.com>
2023-12-29 19:03:45 -0500

    1028.1

--- data.js
+++ data.js
@@ -1237,5 +1237,6 @@ var graf_data= new Map([
 ["1026",{tags:["chloe","bikini","sitting","smug","smiling"],reddit:"18qhf59",twitter:"1739255411805491241",gumroad:"pmvqk",heart:1,i:1231}],
 ["1027",{tags:["chloe","bikini","crouching","feet","smiling"],reddit:"18r88o8",twitter:"1739640641536102440",gumroad:"iemopm",heart:1,i:1232}],
 ["1027.1",{tags:["blonde_girl","rpvoid","hat","tube_top","bikini","skirt","sandals","standing","feet","annoyed","angry","sketch"],reddit:"18rzyy1",twitter:"1739987703499002221",noL:1,i:1233}],
-["1028",{tags:["blonde_girl","rpvoid","bikini","skirt","barefoot_sandals","sitting","feet","smiling"],reddit:"18swi5o",twitter:"1740394253308895324",gumroad:"icdxs",noL:1,i:1234}],
+["1028",{tags:["blonde_girl","rpvoid","bikini","skirt","barefoot_sandals","sitting","feet","smiling"],reddit:"18swi5o",twitter:"1740394253308895324",gumroad:"icdxs",heart:1,i:1234}],
+["1028.1",{tags:["blonde_girl","rpvoid","tube_top","bikini","skirt","standing","surprised","neutral_face","sketch"],reddit:"r/chloe",twitter:"1740782597482180689",noL:1,i:1235}],
 ]);
