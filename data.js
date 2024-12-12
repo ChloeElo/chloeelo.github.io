60cafe00e36d6083ebbae0589fd8a461038b6ef8
ChloeElo <105892909+ChloeElo@users.noreply.github.com>
2024-12-12 18:05:25 -0500

    1124

--- data.js
+++ data.js
@@ -1335,4 +1335,5 @@ var graf_data= new Map([
 ["1121",{tags:["shuten_douji","fate_series","tank_top","horns","feet","sitting","surprised"],reddit:"1h7fluy",twitter:"1864733866985918800",gumroad:"nukpj",heart:1,i:1319}],
 ["1122",{tags:["shimoe_koharu","blue_archive","ponytail","long_hair","tank_top","skirt","feet","crouching","surprised","neutral_face"],reddit:"1hageel",twitter:"1866186951897821237",gumroad:"dbbhcp",heart:1,i:1320}],
 ["1123",{tags:["takanashi_hoshino","blue_archive","ponytail","long_hair","heterochromia","dress","barefoot_sandals","feet","standing","neutral_face"],reddit:"1hc06da",twitter:"1866910730005647604",gumroad:"giuoka",heart:1,i:1321}],
+["1124",{tags:["iochi_mari","blue_archive","cat_ears","long_hair","shirt","feet","sitting","smiling"],reddit:"1hcsg8i",twitter:"1867282174371901801",gumroad:"tozgir",heart:1,i:1322}],
 ]);
